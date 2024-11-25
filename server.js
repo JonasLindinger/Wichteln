e9a6901f2a3ec1c893f756794b97a4b0d576724b
Jonas Lindinger <lindinger.jonas.yt@gmail.com>
2024-11-25 17:29:49 +0100

    updated port

--- server.js
+++ server.js
@@ -17,5 +17,5 @@ const groupRouter = require("./routes/groups");
 app.use("/groups", groupRouter);
 
 // Start the server
-const PORT = 3457;
+const PORT = 3458;
 app.listen(PORT, () => console.log("Server running on http://localhost:" + PORT));
